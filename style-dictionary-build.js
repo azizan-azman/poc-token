c93b1c6bd2db2e48d9bb4034c83064ef15d7308d
Muhammad Azizan bin Azman <mazaz@orsted.com>
2023-08-20 21:26:34 +0800

    setting the config

--- style-dictionary-build.js
+++ style-dictionary-build.js
@@ -1,38 +1,57 @@
-const { registerTransforms } = require("@tokens-studio/sd-transforms");
 const StyleDictionary = require("style-dictionary");
+const { registerTransforms } = require("@tokens-studio/sd-transforms");
 
-registerTransforms(StyleDictionary);
+// sd-transforms, 2nd parameter for options can be added
+// See docs: https://github.com/tokens-studio/sd-transforms
+registerTransforms(StyleDictionary, {
+  expand: {
+    composition: true,
+    typography: false,
+    border: false,
+    shadow: false,
+  },
+  excludeParentKeys: true,
+});
+
+// example value transform, which just returns the token as is
+StyleDictionary.registerTransform({
+  type: "value",
+  name: "myCustomTransform",
+  matcher: (token) => {},
+  transformer: (token) => {
+    return token; // <-- transform as needed
+  },
+});
+
+// format helpers from style-dictionary
+const { fileHeader, formattedVariables } = StyleDictionary.formatHelpers;
+
+// example css format
+StyleDictionary.registerFormat({
+  name: "myCustomFormat",
+  formatter: function ({ dictionary, file, options }) {
+    const { outputReferences } = options;
+    return `
+${formattedVariables({ format: "scss", dictionary, outputReferences })}
+`;
+  },
+});
 
 const sd = StyleDictionary.extend({
-  source: ["tokens/*.tokens.json"],
+  source: ["**/*.tokens.json"],
   platforms: {
-    css: {
-      transforms: [
-        "ts/descriptionToComment",
-        "ts/size/px",
-        "ts/opacity",
-        "ts/size/lineheight",
-        "ts/typography/fontWeight",
-        "ts/resolveMath",
-        "ts/size/css/letterspacing",
-        "ts/typography/css/fontFamily",
-        "ts/typography/css/shorthand",
-        "ts/border/css/shorthand",
-        "ts/shadow/css/shorthand",
-        "ts/color/css/hexrgba",
-        "ts/color/modifiers",
-        "name/cti/kebab",
-      ],
-      buildPath: "build/css/",
+    scss: {
+      transformGroup: "tokens-studio",
+      prefix: "sd",
+      buildPath: "sd-output/scss/",
       files: [
         {
-          destination: "variables.css",
-          format: "css/variables",
+          destination: "_variables.scss",
+          format: "scss/variables",
         },
       ],
     },
   },
 });
-
-sd.cleanAllPlatforms();
+sd.cleanAllPlatforms(); // optionally, cleanup files first..
 sd.buildAllPlatforms();
