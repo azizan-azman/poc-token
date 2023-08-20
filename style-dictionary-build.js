2ad3dda3a1bc5d08c499a9be9a47f7f812b26e4f
Muhammad Azizan bin Azman <mazaz@orsted.com>
2023-08-21 02:19:59 +0800

    created new variable files and disabled the old ones

--- style-dictionary-build.js
+++ style-dictionary-build.js
@@ -13,41 +13,19 @@ registerTransforms(StyleDictionary, {
   excludeParentKeys: true,
 });
 
-// example value transform, which just returns the token as is
-StyleDictionary.registerTransform({
-  type: "value",
-  name: "myCustomTransform",
-  matcher: (token) => {},
-  transformer: (token) => {
-    return token; // <-- transform as needed
-  },
-});
-
-// format helpers from style-dictionary
-const { fileHeader, formattedVariables } = StyleDictionary.formatHelpers;
-
-// example css format
-StyleDictionary.registerFormat({
-  name: "myCustomFormat",
-  formatter: function ({ dictionary, file, options }) {
-    const { outputReferences } = options;
-    return `
-${formattedVariables({ format: "scss", dictionary, outputReferences })}
-`;
-  },
-});
-
 const sd = StyleDictionary.extend({
   source: ["**/*.tokens.json"],
   platforms: {
-    scss: {
-      transformGroup: "tokens-studio",
-      prefix: "sd",
+    css: {
+      transformGroup: "css",
       buildPath: "sd-output/scss/",
       files: [
         {
           destination: "_variables.scss",
           format: "scss/variables",
+          options: {
+            outputReferences: true,
+          },
         },
       ],
     },
