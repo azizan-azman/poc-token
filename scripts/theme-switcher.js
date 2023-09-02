1a7e493373afa7e48723e8b85b89f9b6434ee477
Muhammad Azizan bin Azman <mazaz@orsted.com>
2023-09-02 12:55:19 +0800

    added new sass map for theming

---
+++ scripts/theme-switcher.js
@@ -0,0 +1,34 @@
+// Array of available themes
+const themes = ["casual", "business"];
+
+const body = document.querySelector("body");
+body.setAttribute("class", "theme-casual");
+
+// Function to populate the theme dropdown
+function populateThemeDropdown() {
+  const themeDropdown = document.getElementById("theme");
+
+  themes.forEach((theme) => {
+    const option = document.createElement("option");
+    option.value = theme;
+    option.textContent = theme.charAt(0).toUpperCase() + theme.slice(1);
+    themeDropdown.appendChild(option);
+  });
+}
+
+// Function to change the theme based on the selected option
+function changeTheme(selectedTheme) {
+  const body = document.querySelector("body");
+  body.setAttribute("class", `theme-${selectedTheme}`);
+}
+
+// Event listener for theme dropdown change
+document.addEventListener("DOMContentLoaded", () => {
+  populateThemeDropdown();
+
+  const themeSelector = document.getElementById("theme");
+  themeSelector.addEventListener("change", () => {
+    const selectedTheme = themeSelector.value;
+    changeTheme(selectedTheme);
+  });
+});
